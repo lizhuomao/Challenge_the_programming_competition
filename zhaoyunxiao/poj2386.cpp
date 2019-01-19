5320643977e18edff459cabe4ff911e0bef5908b
赵云霄 <384498377@163.com>
2019-01-19 10:08:22 +0800

    poj2386

--- zhaoyunxiao/poj2386.cpp
+++ zhaoyunxiao/poj2386.cpp
@@ -1,3 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n,m,num=0;
+void dfs(int x,int y,char **array);
 int main(){
+    cin>>n>>m;
+    char **array=new char* [n];
+    for(int i=0;i<n;i++) array[i]=new char[m];
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++) cin>>array[i][j];
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(array[i][j]=='W'){
+                dfs(i,j,array);
+                num++;
+            }
+        }
+    }
+    cout<<num<<endl;
     return 0;
 }
+
+void dfs(int x,int y,char **array){
+    array[x][y]='.';
+    for(int dx=-1;dx<=1;dx++){
+        for(int dy=-1;dy<=1;dy++){
+            int nx=x+dx,ny=y+dy;
+            if(nx>=0 && ny>=0 &&nx<n &&ny<m &&array[nx][ny]=='W')  dfs(nx,ny,array);
+        }
+    }
+}
+
